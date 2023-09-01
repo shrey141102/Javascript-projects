2902dacf05ff73ee7c8386ae7a89acf89435a1de
shrey141102 <shrey14112002@gmail.com>
2023-09-01 17:38:41 +0530

    calc

---
+++ Calculator/script.js
@@ -0,0 +1,36 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.slice(0, -1)
+            input.value = string;
+        }
+        else if(e.target.innerHTML == '÷')
+        {
+            string += '/';
+            input.value = string;
+        }
+        else if(e.target.innerHTML == '×')
+        {
+            string += '*';
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
