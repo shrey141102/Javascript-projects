ab891ae8d3227b2b245b8936add22173d5b03a33
SRUJANA2199 <136368774+SRUJANA2199@users.noreply.github.com>
2023-10-01 13:06:03 +0530

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,49 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero"
+
+while True:
+    # Menu
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+
+    choice = input("Enter choice (1/2/3/4/5): ")
+
+    if choice == '5':
+        print("Exiting the calculator. Goodbye!")
+        break
+
+    if choice not in ('1', '2', '3', '4'):
+        print("Invalid choice. Please enter a number between 1 and 5.")
+        continue
+
+    # Get user input for numbers
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        print(f"{num1} + {num2} = {add(num1, num2)}")
+
+    elif choice == '2':
+        print(f"{num1} - {num2} = {subtract(num1, num2)}")
+
+    elif choice == '3':
+        print(f"{num1} * {num2} = {multiply(num1, num2)}")
+
+    elif choice == '4':
+        print(f"{num1} / {num2} = {divide(num1, num2)}")
