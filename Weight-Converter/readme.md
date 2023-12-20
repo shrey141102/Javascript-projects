c9413a0feacef70c1304dad043887912134f56f3
Baron105 <barunparua@gmail.com>
2023-12-20 09:08:15 +0530

    updated readme

---
+++ Weight-Converter/readme.md
@@ -0,0 +1,11 @@
+# Weight Converter
+
+## Description
+
+This is a simple weight converter that converts supports conversion between the units Kilograms, Stones, Pounds and Ounces and vice versa. It is built using HTML, CSS and JavaScript.
+
+## How to use
+
+Enter the weight in the suitable input field and the conversion will be done automatically without the need to click any button. The conversion will be done in real time as you type.
+
+The formula used for the conversion is also displayed below the input fields. Also please note that while negative values are allowed, the conversion only makes sense for positive values.
--- Weight-Converter/script.js
+++ Weight-Converter/script.js
@@ -1,64 +1,95 @@
-function MtF(valNum) {
-    document.getElementById("outputFeet").innerHTML = valNum * 3.2808;
-  }
-  
-  function FtM(valNum) {
-    document.getElementById("outputMeters").innerHTML = valNum / 3.2808;
-  }
-  
-  function CtI(valNum) {
-    document.getElementById("outputInches").innerHTML = valNum * 0.39370;
-  }
-  
-  function ItC(valNum) {
-    document.getElementById("outputcm").innerHTML = valNum / 0.39370;
-  }
-  
-  function MilesToKm(valNum) {
-    document.getElementById("outputKm").innerHTML = valNum * 1.60934;
-  }
-  
-  function KmToMiles(valNum) {
-    document.getElementById("outputMiles").innerHTML = valNum / 1.60934;
-  }
-  
-  function MetersToYards(valNum) {
-    document.getElementById("outputYards").innerHTML = valNum * 1.09361;
-  }
-  
-  function YardsToMeters(valNum) {
-    document.getElementById("outputMetersYard").innerHTML = valNum / 1.09361;
-  }
-  
-  document.getElementById("inputFeet").addEventListener("input", function() {
-    FtM(this.value);
-  });
-  
-  document.getElementById("inputMetres").addEventListener("input", function() {
-    MtF(this.value);
-  });
-  
-  document.getElementById("inputcm").addEventListener("input", function() {
-    CtI(this.value);
-  });
-  
-  document.getElementById("inputInches").addEventListener("input", function() {
-    ItC(this.value);
-  });
-  
-  document.getElementById("inputMiles").addEventListener("input", function() {
-    MilesToKm(this.value);
-  });
-  
-  document.getElementById("inputKm").addEventListener("input", function() {
-    KmToMiles(this.value);
-  });
-  
-  document.getElementById("inputMetersYard").addEventListener("input", function() {
-    MetersToYards(this.value);
-  });
-  
-  document.getElementById("inputYards").addEventListener("input", function() {
-    YardsToMeters(this.value);
-  });
-  
+function KgToPounds(valNum) {
+  document.getElementById("outputPounds").innerHTML = valNum * 2.20462;
+}
+
+function PoundsToKg(valNum) {
+  document.getElementById("outputKg").innerHTML = valNum / 2.20462;
+}
+
+function KgToOunces(valNum) {
+  document.getElementById("outputOz").innerHTML = valNum * 35.274;
+}
+
+function OuncesToKg(valNum) {
+  document.getElementById("outputKgOz").innerHTML = valNum / 35.274;
+}
+
+function KgToStones(valNum) {
+  document.getElementById("outputStones").innerHTML = valNum * 0.157473;
+}
+
+function StonesToKg(valNum) {
+  document.getElementById("outputKgStones").innerHTML = valNum / 0.157473;
+}
+
+document.getElementById("inputKg").addEventListener("input", function() {
+  KgToPounds(this.value);
+});
+
+document.getElementById("inputPounds").addEventListener("input", function() {
+  PoundsToKg(this.value);
+});
+
+document.getElementById("inputKgOz").addEventListener("input", function() {
+  KgToOunces(this.value);
+});
+
+document.getElementById("inputOzKg").addEventListener("input", function() {
+  OuncesToKg(this.value);
+});
+
+document.getElementById("inputKgStones").addEventListener("input", function() {
+  KgToStones(this.value);
+});
+
+document.getElementById("inputStonesKg").addEventListener("input", function() {
+  StonesToKg(this.value);
+});
+
+function PoundsToOunces(valNum) {
+  document.getElementById("outputPoundsOz").innerHTML = valNum * 16;
+}
+
+function OuncesToPounds(valNum) {
+  document.getElementById("outputOzPounds").innerHTML = valNum / 16;
+}
+
+function PoundsToStones(valNum) {
+  document.getElementById("outputPoundsStones").innerHTML = valNum / 14;
+}
+
+function StonesToPounds(valNum) {
+  document.getElementById("outputStonesPounds").innerHTML = valNum * 14;
+}
+
+function OuncesToStones(valNum) {
+  document.getElementById("outputOzStones").innerHTML = valNum / 224;
+}
+
+function StonesToOunces(valNum) {
+  document.getElementById("outputStonesOz").innerHTML = valNum * 224;
+}
+
+document.getElementById("inputPoundsOz").addEventListener("input", function() {
+  PoundsToOunces(this.value);
+});
+
+document.getElementById("inputOzPounds").addEventListener("input", function() {
+  OuncesToPounds(this.value);
+});
+
+document.getElementById("inputPoundsStones").addEventListener("input", function() {
+  PoundsToStones(this.value);
+});
+
+document.getElementById("inputStonesPounds").addEventListener("input", function() {
+  StonesToPounds(this.value);
+});
+
+document.getElementById("inputOzStones").addEventListener("input", function() {
+  OuncesToStones(this.value);
+});
+
+document.getElementById("inputStonesOz").addEventListener("input", function() {
+  StonesToOunces(this.value);
+});
